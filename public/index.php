e38ee6a3b7c70e466577e2a236a6d9f6d95a53ab
eenattayani <enryfirmana@gmail.com>
2025-09-09 11:47:09 +0700

    link github to hositnger

--- public/index.php
+++ public/index.php
@@ -4,14 +4,26 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
+
+// ---- untuk di hosting ----//
+// define('LARAVEL_START', microtime(true));
+
+// require __DIR__.'/../../laravel_b3/vendor/autoload.php';
+
+// $app = require_once __DIR__.'/../../laravel_b3/bootstrap/app.php';
+
+// $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+// $response = $kernel->handle(
+//     $request = Illuminate\Http\Request::capture()
+// )->send();
+
+// $kernel->terminate($request, $response);
